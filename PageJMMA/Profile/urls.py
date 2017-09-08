e85a87ae8356df3d70493d7bebd211df647c1cd6
juanmacedo <juanmacedo@SCTMAC02.com.sweetcherrytech>
2017-09-08 17:01:10 -0400

    Linking templates

--- PageJMMA/Profile/urls.py
+++ PageJMMA/Profile/urls.py
@@ -3,4 +3,10 @@ from Profile import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'^projects/$', views.ProjectsPageView.as_view()),
+    url(r'^tutorials/$', views.TutorialsPageView.as_view()),
+    url(r'^videos/$', views.VideosPageView.as_view()),
+    url(r'^languages/$', views.LanguagesPageView.as_view()),
+    url(r'^articles/$', views.ArticlesPageView.as_view()),
+  
 ]
--- PageJMMA/Profile/views.py
+++ PageJMMA/Profile/views.py
@@ -5,3 +5,18 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+class ProjectsPageView(TemplateView):
+    template_name = 'projects.html'
+
+class TutorialsPageView(TemplateView):
+    template_name = 'tutorials.html'
+
+class ArticlesPageView(TemplateView):
+    template_name = 'articles.html'
+
+class LanguagesPageView(TemplateView):
+    template_name = 'languages.html'
+
+class VideosPageView(TemplateView):
+    template_name = 'videos.html'
