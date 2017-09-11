412d868aa4779c1a7f95a6d580b61da06ba711b0
juanmacedo <juanmacedo@SCTMAC02.com.sweetcherrytech>
2017-09-11 14:26:38 -0400

    Add articles

--- PageJMMA/Profile/urls.py
+++ PageJMMA/Profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from Profile import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'^index/$', views.HomePageView.as_view()),
     url(r'^projects/$', views.ProjectsPageView.as_view()),
     url(r'^tutorials/$', views.TutorialsPageView.as_view()),
     url(r'^videos/$', views.VideosPageView.as_view()),
