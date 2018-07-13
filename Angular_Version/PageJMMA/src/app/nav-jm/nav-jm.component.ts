3c2982674dc465f8551f4bcfc334340e97bf6e48
Juan Manuel Macedo Almeida <jmmacedo@vectoritcgroup.com>
2018-07-13 11:14:19 +0200

    styles add

--- Angular_Version/PageJMMA/src/app/nav-jm/nav-jm.component.ts
+++ Angular_Version/PageJMMA/src/app/nav-jm/nav-jm.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { pagesJM } from 'src/assets/objects/pagesJM';
 
 @Component({
   selector: 'nav-jm',
   templateUrl: './nav-jm.component.html',
-  styleUrls: ['./nav-jm.component.css']
+  styleUrls: ['./nav-jm.component.scss']
 })
 export class NavJMComponent {
+
+  public pagesJM = pagesJM;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
+
 
 }
---
+++ Angular_Version/PageJMMA/src/assets/objects/pagesJM.ts
@@ -0,0 +1,23 @@
+export const pagesJM = [
+  {
+    title: 'Home',
+    route: './home'
+  },
+  {
+    title: 'Backgrounds',
+    route: './backgrounds'
+  },
+  {
+    title: 'Curriculum',
+    route: './curriculum'
+  },
+  {
+    title: 'Contact',
+    route: './contact'
+  },
+  {
+    title: 'Media',
+    route: './media'
+  }
+];
+
